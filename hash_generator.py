7d578c051a099c08af726696275894d52a8ecb0b
nurbadalova <nurbadalova75@gmail.com>
2026-07-13 18:38:30 +0400

    Add user choice for hash generation options

--- hash_generator.py
+++ hash_generator.py
@@ -1,5 +1,13 @@
 import hashlib
 
+print("===== HASH GENERATOR =====")
+print("1. MD5")
+print("2. SHA1")
+print("3. SHA256")
+print("4. Generate All")
+
+choice = input("\nChoose an option (1-4): ")
+
 text = input("Enter text: ")
 
 text_bytes = text.encode()
@@ -8,7 +16,20 @@ md5_hash = hashlib.md5(text_bytes).hexdigest()
 sha1_hash = hashlib.sha1(text_bytes).hexdigest()
 sha256_hash = hashlib.sha256(text_bytes).hexdigest()
 
-print("\nGenerated Hashes:")
-print("MD5    :", md5_hash)
-print("SHA1   :", sha1_hash)
-print("SHA256 :", sha256_hash)
+if choice == "1":
+    print("\nMD5:", md5_hash)
+
+elif choice == "2":
+    print("\nSHA1:", sha1_hash)
+
+elif choice == "3":
+    print("\nSHA256:", sha256_hash)
+
+elif choice == "4":
+    print("\nGenerated Hashes:")
+    print("MD5    :", md5_hash)
+    print("SHA1   :", sha1_hash)
+    print("SHA256 :", sha256_hash)
+
+else:
+    print("\nInvalid option!")
