9e7abd323f33a624bda3a65c36c0032c002b5f47
nurbadalova <nurbadalova75@gmail.com>
2026-07-13 18:08:05 +0400

    Add hash generator script
    Add functionality to generate MD5, SHA1, and SHA256 hashes from user input.

--- hash_generator.py
+++ hash_generator.py
@@ -1,1 +1,14 @@
+import hashlib
 
+text = input("Enter text: ")
+
+text_bytes = text.encode()
+
+md5_hash = hashlib.md5(text_bytes).hexdigest()
+sha1_hash = hashlib.sha1(text_bytes).hexdigest()
+sha256_hash = hashlib.sha256(text_bytes).hexdigest()
+
+print("\nGenerated Hashes:")
+print("MD5    :", md5_hash)
+print("SHA1   :", sha1_hash)
+print("SHA256 :", sha256_hash)
